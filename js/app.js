44f5330721ecb078483d0c3debcd993640b32591
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-06-27 12:59:52 -0500

    project update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const menuIcon = document.querySelector("#menu-icon");
 const links = document.querySelectorAll('.link-text');
 const sidebar = document.querySelector('.layout-sidebar');
-const headers = document.querySelectorAll('.headers');
+const headerTitle = document.querySelector('.sidebar__header__title');
+const headerTitleMenu = document.querySelector('.navigation__categorias_title');
 const linkItems = document.querySelectorAll('.sidebar li a');
 
 function showSidebar(){
     links.forEach(link=>link.classList.toggle('is-text-contracted'));
     sidebar.classList.toggle('is-layout-contracted');
-    headers.forEach(header=>header.classList.toggle('is-headers-contracted'))
-    
+    headerTitle.classList.toggle('is-headers-contracted');
+    headerTitleMenu.classList.toggle('is-title-hidden');
 }
 
 function activeItem(){
